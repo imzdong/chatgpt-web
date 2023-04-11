49743a6b5909b46afd7f975460ac2bee92c67d92
Winter <zhoudong_dream@163.com>
2023-04-11 23:12:29 +0800

    app 100%

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,17 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* 设置 App.vue 的高度为 100% */
+html, body, #app {
+  height: 100%;
+  background-color: aqua;
+  padding:0px;
+  margin:0px;
+  position:absolute;
+  top:0px;
+  left:0px;
+  width:100%;
+  height:100%;
+  border:hidden;
 }
 </style>
