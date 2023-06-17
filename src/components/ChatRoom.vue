1e0ca61fcbb5fdc410a59745db521ac34738b2a6
Winter <zhoudong_dream@163.com>
2023-06-17 21:53:51 +0800

    添加room

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -8,13 +8,23 @@
       >
         <el-form :model="form" ref="form" label-width="100px">
           <el-form-item label="Name">
-            <el-input v-model="form.roomName"></el-input>
+            <el-input v-model="form.roomName" style="width:80%"></el-input>
+          </el-form-item>
+          <el-form-item label="Model" >
+            <el-select v-model="form.model" style="width:80%" placeholder="Select" size="large">
+              <el-option
+                v-for="item in modelList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button @click="closeFormDialog">取消</el-button>
+          <el-button type="primary" @click="submitForm">确定</el-button>
           </el-form-item>
         </el-form>
-        <div class="dialog-footer">
-          <el-button @click="closeFormDialog">取消</el-button>
-          <el-button type="primary" @click="submitForm">确定</el-button>
-        </div>
       </el-dialog>
     </div>
   </template>
@@ -26,8 +36,14 @@
       return {
         dialogVisible: false,
         form: {
-          roomName: ''
-        }
+          roomName: '',
+          model: '',
+        },
+        modelList:[
+        {label: 'gpt-3.5', value: 'gpt-3.5-v'},
+        {label: 'gpt-4', value: 'gpt-4-v'},
+        {label: 'code', value: 'code-v'},
+        ],
       };
     },
     methods: {
@@ -42,8 +58,14 @@
       submitForm() {
         // 处理表单提交逻辑，例如将数据发送到服务器或添加到聊天室列表
         const newRoom = {
-          id: Math.random(),
-          name: this.form.roomName
+          roomId: Math.random(),
+          roomName: this.form.roomName,
+          model: this.form.model,
+          avatar: 'https://www.flaticon.com/free-icons/english-bulldog',
+					users: [
+						{ _id: '1234', username: 'John Doe' },
+						{ _id: '4321', username: 'John Snow' }
+					]
         };
         this.$emit('child-click', newRoom);
         this.closeFormDialog();
